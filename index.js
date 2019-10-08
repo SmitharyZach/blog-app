443deb6cb2335851859fdcdaccbac9217b1376c5
SmitharyZach <zach@calendly.com>
2019-10-07 21:24:00 -0400

    extracting blog schema into model

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = 'mongodb+srv://full_stack_zach:attack1993@cluster0-azqkn.mongodb.net/note-app?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongodDB', error.message)
+  })
+
 
 app.use(cors())
 app.use(bodyParser.json())
